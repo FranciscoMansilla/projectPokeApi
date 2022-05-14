7c8e750ef15595730d43d720cc2b4f72d34123bd
Fran999 <franciscomansilla1999@gmail.com>
2022-05-14 16:54:09 -0300

    logica de sort y filtro de existencia terminado

--- client/src/components/OpcionBar/OpcionBar.jsx
+++ client/src/components/OpcionBar/OpcionBar.jsx
@@ -1,10 +1,73 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllPokemons, getAllTypes } from "../../store/actions";
 import { Link } from "react-router-dom";
 
 export default function OpcionBar (){
-  // let dispatch = useDispatch()
+  let dispatch = useDispatch()
+  let pokemons = useSelector((state)=>state.pokemons)
+
+  
+  const onChangeSort = (e)=>{
+    let value = e.target.value
+    if(value==="A-Z"){
+      let pokeSort =pokemons
+      pokeSort.sort(function (a, b) {
+        if (a.name > b.name) {
+          return 1;}
+        if (a.name < b.name) {
+          return -1;}
+        return 0;
+      });
+      console.log(pokeSort)
+    }
+    if(value==="Z-A"){
+      let pokeSort =pokemons
+      pokeSort.sort(function (a, b) {
+        if (a.name < b.name) {
+          return 1;}
+        if (a.name > b.name) {
+          return -1;}
+        return 0;
+      });
+      console.log(pokeSort)
+    }
+    if(value==="Ascendent"){
+      let pokeSort =pokemons
+      pokeSort.sort(function (a, b) {
+        if (a.hp > b.hp) {
+          return 1;}
+        if (a.hp < b.hp) {
+          return -1;}
+        return 0;
+      });
+      console.log(pokeSort)
+    }
+    if(value==="Descendent"){
+      let pokeSort =pokemons
+      pokeSort.sort(function (a, b) {
+        if (a.hp < b.hp) {
+          return 1;}
+        if (a.hp > b.hp) {
+          return -1;}
+        return 0;
+      });
+      console.log(pokeSort)
+    }
+  }
+  const onChangeFilterByOrigin = (e)=>{
+    if(e.target.value==='existing'){
+      let pokefilter = pokemons.filter(p=>p.id>=1 && p.id<=40)
+      console.log(pokefilter)
+    }
+    if(e.target.value==='created'){
+      let pokefilter = pokemons.filter(p=>!(p.id>=1 && p.id<=40))
+      console.log(pokefilter)
+    }
+    if(e.target.value==='all'){
+      console.log(pokemons)
+    }
+  }
   // useEffect(()=>{
   //   dispatch(getAllPokemons())
   //   dispatch(getAllTypes())
@@ -12,7 +75,8 @@ export default function OpcionBar (){
   return(
     <div>
       <label for="filter" class="">Sort by:</label>
-      <select name="filter" class="">
+      <select onChange={onChangeSort} name="filter" class="">
+        <option  value="select"></option>
         <optgroup label="By name:">
           <option value="A-Z">A-Z</option>
           <option value="Z-A">Z-A</option>
@@ -24,7 +88,8 @@ export default function OpcionBar (){
       </select>
 
       <label for="filter" class="">Filter by Origin:</label>
-      <select name="filter" class="">
+      <select onChange={onChangeFilterByOrigin} name="filter" class="">
+      <option  value="select"></option>
         <option value="all">All Pokemons</option>
         <option value="existing">Existing Pokemons</option>
         <option value="created">Created by User</option>
