96e5664c1dd8556a78324156ad57bc6c961113fe
Fran999 <franciscomansilla1999@gmail.com>
2022-05-18 15:18:26 -0300

    css

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -3,11 +3,12 @@ import './Loading.css'
 export default function Loading(){
 
   return(
-    <div className='divGif_w8r7'>
-      <div className='' >
+    <div className='' >
+        <div className='divGif_w8r7'>
         {/* <img src="https://thumbs.gfycat.com/BareDenseFallowdeer-max-1mb.gif" alt="loading" /> */}
         {/* <img src="https://i.gifer.com/Pm6k.gif" alt="" /> */}
         <img src="https://pa1.narvii.com/7191/b9ab239fa43eae179fd353c63c76de863ea83ed6r1-370-296_hq.gif" alt="" />
+        <label className='labelLoading_78qw'><b>Loading...</b></label>
       </div>
     </div>
   )
